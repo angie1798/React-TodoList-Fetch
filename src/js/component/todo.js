79abe482e560051e83c02f4b077f4155435e1b15
angie1798 <angie179836@gmail.com>
2021-03-15 22:41:20 +0000

    Todo con fetch api terminado

--- src/js/component/todo.js
+++ src/js/component/todo.js
@@ -1,27 +1,109 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const Todo = () => {
-	let [counter, setCounter] = useState(0);
 	const [tasks, setTasks] = useState("");
 	const [list, setList] = useState([]);
 
+	useEffect(() => {
+		getList();
+	}, []);
+
+	//funcion get para UseEffect
+	const getList = () => {
+		let fetchUrl = "https://assets.breatheco.de/apis/fake/todos/user/angie";
+		fetch(fetchUrl)
+			.then(response => response.json())
+			.then(fetchBody => {
+				setList(
+					fetchBody.map(item => {
+						return { label: item.label, done: item.done };
+					})
+				);
+			})
+			.catch(error => console.log("error", error));
+	};
+
+	//funcion put para fetch
+	const putFetch = listTaskNew => {
+		var myHeaders = new Headers();
+		myHeaders.append("Content-Type", "application/json");
+
+		var raw = JSON.stringify(listTaskNew);
+
+		var requestOptions = {
+			method: "PUT",
+			headers: myHeaders,
+			body: raw,
+			redirect: "follow"
+		};
+
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/angie",
+			requestOptions
+		)
+			.then(response => response.json())
+			.then(result => {
+				console.log(result);
+				alert(result.result);
+			})
+			.catch(error => console.log("error", error));
+	};
+
+	//metodo eliminar
 	const deleteTask = indexDelete => {
 		let resultado = list.filter((tasks, index) => index != indexDelete);
 		setList(resultado);
-		setCounter(counter - 1);
+		//se actualiza la lista en el api
+		putFetch(resultado);
 	};
 
+	//funcion onChange para controlar lo que se va escribiendo en tasks
 	function agregar(e) {
 		setTasks(e);
 	}
+	//funcion para agregar task a la lista de tareas
 	const agregarLista = e => {
 		if (tasks != "") {
 			if (e.key == "Enter") {
-				setCounter(counter + 1);
-				setList([...list, tasks]);
+				setList([...list, { label: tasks, done: false }]);
+				//se actualiza la lista de la api
+				console.log(list);
+				//putFetch(list);
 				setTasks("");
 			}
 		}
+	};
+
+	//funcion para eliminar todos los elementos de la lista
+	const eliminarTodo = () => {
+		var requestOptions = {
+			method: "DELETE",
+			redirect: "follow"
+		};
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/angie",
+			requestOptions
+		)
+			.then(response => response.json())
+			.then(result => console.log(result))
+			.then(newTodo())
+			.catch(error => console.log("error", error));
+	};
+
+	//funcion para metodo post, luego de hacer delete all
+	const newTodo = () => {
+		let array = [];
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/angie", {
+			method: "POST",
+			body: JSON.stringify(array),
+			headers: { "Content-Type": "application/json" }
+		})
+			.then(res => res.json())
+			.then(data => {
+				console.log("newToDo", data);
+				getList();
+			}) //cargando la lista
+			.catch(error => console.error("Error:", error.message));
 	};
 
 	return (
@@ -43,7 +125,7 @@ export const Todo = () => {
 							key={index}
 							className="list-group-item"
 							id="distintor">
-							{item}
+							{item.label}
 							<i
 								className="fas fa-times"
 								onClick={() => {
@@ -55,12 +137,28 @@ export const Todo = () => {
 				<li className="list-group-item">
 					<small>
 						{"" +
-							(counter == 0
+							(list.length == 0
 								? "No tasks, add a task"
-								: counter + " items left")}
+								: list.length + " items left")}
 					</small>
 				</li>
 			</ul>
+			<div id="hoja1" className="shadow"></div>
+			<div id="hoja2" className="shadow"></div>
+			<button
+				className="btn btn-secondary"
+				id="del"
+				onClick={eliminarTodo}>
+				Delete all elements on the list
+			</button>
+			<button
+				type="button"
+				className="btn btn-rosa"
+				onClick={() => {
+					putFetch(list);
+				}}>
+				Update List
+			</button>
 		</div>
 	);
 };
